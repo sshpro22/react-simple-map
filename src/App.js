d72b321ee4c76af758f7cc9bb7845fa88f4ec9bd
sshpro22 <sshpro22@gmail.com>
2018-11-23 05:58:16 -0800

    Feat: Render the initial map

--- src/App.js
+++ src/App.js
@@ -1,28 +1,53 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
+
+  componentDidMount () {
+    this.renderMap()
+  }
+  renderMap = () => {
+    //Call the loadAPIScript function with the script url containing the API key
+    loadAPIScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyCf3HZWXQWd8cc2scByGMv0EgACrFNjMMM&callback=initMap")
+    window.initMap = this.initMap;
+  }
+  initMap = () => {
+    const map = new window.google.maps.Map(document.getElementById("map"), {
+      center: {
+        lat: -34.397,
+        lng: 150.644
+      },
+      zoom: 8
+    });
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <main>
+        <div id="map" />
+      </main>
     );
   }
+}
+
+/*
+ * The loadAPIScript gets the url. It creates a scrript tag
+ * with a src attribute set to the url provided and async and defer set
+ * to true.
+ * index.parentNode.insertBefore(script, index) : Instead of appendChild.
+ * [script: newNode
+ * index: referenceNode]
+ * With "index" we select the first script tag, with "parentNode"
+ * we select the parent node, with "insertBefore" we put our script
+ * at the very beginning, to the top of the lists.
+ */
+
+function loadAPIScript(url) {
+  let index = window.document.getElementsByTagName("script")[0];
+  let script = window.document.createElement("script");
+  script.src = url;
+  script.async = true;
+  script.defer = true;
+  index.parentNode.insertBefore(script, index);
 }
 
 export default App;
